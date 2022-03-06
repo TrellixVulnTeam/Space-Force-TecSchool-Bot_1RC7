da842467f4fc55070b0147f6f79ccae5c1925097
UtCrypticiores <ut.crypticiores@gmail.com>
2022-03-05 18:51:35 -0800

    info update

--- src/commands/info.js
+++ src/commands/info.js
@@ -4,6 +4,6 @@ module.exports = {
 		.setName("info")
 		.setDescription("Gives general info"),
 	async execute(interaction) {
-		await interaction.reply("Visit the Github for all of the code\nhttps://github.com/UtCrypticiores/TSgt-Rock.git")
+		await interaction.reply("Visit the Github for all of the code\nhttps://github.com/UtCrypticiores/Space-Force-TecSchool-Bot.git")
 	},
 };
