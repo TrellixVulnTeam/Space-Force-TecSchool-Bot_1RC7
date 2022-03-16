2586379eeaa0df60dc021b14544fd47d11b52bb9
UtCrypticiores <ut.crypticiores@gmail.com>
2022-03-15 18:22:34 -0700

    run

--- run.sh
+++ run.sh
@@ -10,5 +10,7 @@ while : ; do
 done
 
 sudo apt-get update && sudo apt-get upgrade -y
+sudo git stash
 git config pull.ff only
 git pull https://github.com/UtCrypticiores/Space-Force-TecSchool-Bot.git
+sudo chmod 744 /home/pi/Space-Force-TecSchool-Bot/run.sh
