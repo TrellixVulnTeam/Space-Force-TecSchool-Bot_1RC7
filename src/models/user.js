e4f0c1bc72bf19928006d79d883ad592196ea721
UtCrypticiores <68932213+UtCrypticiores@users.noreply.github.com>
2022-07-09 09:58:42 -0600

    npm update

--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     id: { type: String, required: true },
     username: { type: String, required: true },
-    guilds: { type: Array, required: true },
     icon: { type: String, required: true },
+    guilds: { type: Array, required: true },
 });
 
 const User = (module.exports = mongoose.model("User", UserSchema));
--- src/strategies/discord.js
+++ src/strategies/discord.js
@@ -3,22 +3,21 @@ require("dotenv").config();
 const passport = require("passport");
 const path = require("path");
 const error = require(path.join(__dirname, "../modules/error/error"));
-const general = require(path.join(__dirname, "../modules/general"));
+const DiscordUser = require("../models/user");
 
-// passport.serializeUser((user, done) => {
-//     done(null, user.id);
-// });
-// passport.deserializeUser(async (id, done) => {
-//     // try {
-//     //     const user = await User.findById(id);
-//     //     if (user) {
-//     //         done(null, user);
-//     //     }
-//     // } catch (err) {
-//     //     error.error(1, err);
-//     // }
-// });
-
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await DiscordUser.findById(id);
+        if (user) {
+            done(null, user);
+        }
+    } catch (err) {
+        error.error(2, err);
+    }
+});
 try {
     passport.use(
         new DiscordStrategy(
@@ -29,14 +28,45 @@ try {
                 scope: ["identify", "guilds"],
             },
             async (accessToken, refreshToken, profile, done) => {
-                console.log(`${general.time}${profile.id}`);
-                console.log(`${general.time}${refreshToken}`);
-                console.log(`${general.time}${accessToken}`);
-              return  done()
+                console.log(profile);
+                const user = await DiscordUser.findOne({
+                    id: profile.id,
+                });
+
+                if (user) {
+                    if (
+                        user.id == profile.id &&
+                        user.username == profile.username &&
+                        user.guilds == profile.guilds &&
+                        user.icon == profile.avatar
+                    ) {
+                        done(null, user);
+                    } else {
+                        const updateUser = await DiscordUser.updateOne(
+                            { id: profile.id },
+                            {
+                                id: profile.id,
+                                username: profile.username,
+                                guilds: profile.guilds,
+                                icon: profile.avatar,
+                            }
+                        );
+                        done(null, user);
+                    }
+                } else {
+                    const newUser = await DiscordUser.create({
+                        id: profile.id,
+                        username: profile.username,
+                        guilds: profile.guilds,
+                        icon: profile.avatar,
+                    });
+                    const savedUser = await newUser.save();
+                    done(null, savedUser);
+                }
             }
         )
     );
 } catch (err) {
-    error.error(1, err);
+    error.error(2, err);
     done(err, null);
 }
